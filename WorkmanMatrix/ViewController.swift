2fb4be0f80ce613ec9c785d77bc2fa3c123c4c34
Tom Whiston <tom.whiston@gmail.com>
2019-03-29 19:31:16 +0100

    Initial Commit

---
+++ WorkmanMatrix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkmanMatrix
+//
+//  Created by twhiston on 29.03.19.
+//  Copyright © 2019 twhiston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorkmanMatrixTests/WorkmanMatrixTests.swift
@@ -0,0 +1,34 @@
+//
+//  WorkmanMatrixTests.swift
+//  WorkmanMatrixTests
+//
+//  Created by twhiston on 29.03.19.
+//  Copyright © 2019 twhiston. All rights reserved.
+//
+
+import XCTest
+@testable import WorkmanMatrix
+
+class WorkmanMatrixTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
